c9bb3a8ff01371e3132f6c579464368e719be91a
swati07sharma <103050981+swati07sharma@users.noreply.github.com>
2022-11-25 14:38:47 +0530

    Add files via upload

--- core/src/com/tankstars/screens/p2/ChooseTankAbramsP2.java
+++ core/src/com/tankstars/screens/p2/ChooseTankAbramsP2.java
@@ -24,7 +24,7 @@ class ChooseTankAbramsP2 implements Screen {
 
     @Override
     public void show() {
-        img = new Texture("Choose Tank - P1 - 1.jpg");
+        img = new Texture("Choose Tank - P2 - 1.png");
     }
 
     @Override
@@ -34,6 +34,50 @@ class ChooseTankAbramsP2 implements Screen {
     @Override
     public void render(float delta) {
         game.batch.begin();
+
+        int x = 1468;
+        int y = 761;
+
+        int a = 1386;
+        int b = 32;
+
+        int ax = 1438;
+        int ay = 467;
+
+        int bx = 1578;
+        int by = 467;
+
+        int cx = 1718;
+        int cy = 467;
+
+        if ( (Gdx.input.getX() > a && Gdx.input.getX() < (a + RADIUS) ) && (Gdx.input.getY() > b && Gdx.input.getY() < (b + RADIUS) ) ) {
+            if (Gdx.input.isTouched()) {
+                this.dispose();
+                game.setScreen(new MainScreen(game));
+            }
+        }
+        else if ( (Gdx.input.getX() > x && Gdx.input.getX() < (x + WIDTH) ) && (Gdx.input.getY() > y && Gdx.input.getY() < (y + HEIGHT) ) ) {
+            if (Gdx.input.isTouched()) {
+                this.dispose();
+                game.setScreen(new com.tankstars.screens.Arena(game));
+            }
+        }
+        else if ( (Gdx.input.getX() > bx && Gdx.input.getX() < (bx + RADIUS1) ) && (Gdx.input.getY() > by && Gdx.input.getY() < (by + RADIUS1) ) ) {
+            if (Gdx.input.isTouched()) {
+                this.dispose();
+                game.setScreen(new com.tankstars.screens.p2.ChooseTankMark1P2(game));
+            }
+        }
+        else if ( (Gdx.input.getX() > cx && Gdx.input.getX() < (cx + RADIUS1) ) && (Gdx.input.getY() > cy && Gdx.input.getY() < (cy + RADIUS1) ) ) {
+            if (Gdx.input.isTouched()) {
+                this.dispose();
+                game.setScreen(new com.tankstars.screens.ChooseTankCoalitionP2(game));
+            }
+        }
+        else{
+            game.batch.draw(img, 0,0,1920,1080);
+        }
+
 
         game.batch.end();
     }
