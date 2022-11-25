308c2148e650b14f1bd18b3c3d7c6ba31144ea8e
latentghost <akshat21515@iiitd.ac.in>
2022-11-25 16:42:51 +0530

    Main

---
+++ core/src/com/tankstars/Main.java
@@ -0,0 +1,33 @@
+package com.tankstars;
+
+public class Main {
+    
+    private static Player p1;
+    private static Player p2;
+    private static Drops drop;
+    private static final float gr_h;
+
+
+    public static void play(){
+        // main method to be called that starts the entire game
+    }
+
+    public static float getgr(){
+        return this.gr_h;
+    }
+
+    public static Player getp1(){
+        return this.p1;
+    }
+
+    public static Player getp2(){
+        return this.p2;
+    }
+
+    public static void pushdrop(int x){
+
+    }
+
+
+
+}
